36a9adb1b179d3fa7a2ec075183002525697a9db
Martin <martinsierra@gmail.com>
2016-09-22 21:11:17 -0700

    Implemented Unit Test for PopularMovie class.

--- app/src/test/java/com/themartinsierra/popularmovieapp/ExampleUnitTest.java
+++ app/src/test/java/com/themartinsierra/popularmovieapp/ExampleUnitTest.java
@@ -1,15 +1,41 @@
 package com.themartinsierra.popularmovieapp;
 
-import org.junit.Test;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
+    @Mock
+    Context mMockContext;
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void test() throws Exception {
+        when(mMockContext.getString(R.string.image_size)).thenReturn("w185");
+        when(mMockContext.getString(R.string.image_path)).thenReturn("http://image.tmdb.org/t/p/");
+
+        String ov = "After the re-emergence of the world's first mutant, world-destroyer Apocalypse, the X-Men must unite to defeat his extinction level plan.";
+        PopularMovie pm = new PopularMovie("Captain America: Civil War", "/zSouWWrySXshPCT4t3UKCQGayyo.jpg", ov, "6.84", "2016-05-18", mMockContext);
+
+        assertEquals("Captain America: Civil War",pm.originalTitle);
+        assertEquals("http://image.tmdb.org/t/p/w185/zSouWWrySXshPCT4t3UKCQGayyo.jpg",pm.moviePosterPath);
+        assertEquals(ov,pm.overview);
+        assertEquals("6.84",pm.voteAverage);
+        assertEquals("2016-05-18",pm.releaseDate);
+
     }
 }
