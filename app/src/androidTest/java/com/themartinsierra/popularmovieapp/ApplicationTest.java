a7a565444beb710723a2a62be837ca82cf6cfb20
ram-2020 <s.ramaswamysubramanian@gmail.com>
2016-09-23 23:44:07 -0700

    Added two new tests and Updated one existing test 1. MainActivitySettingsChangeTest - To test the settings menu display "Top Rated" movies 2. SettingsActivityTest - To test the settings menu display "Most Popular" movies 3. ApplicationTest - Updated the test to assert the selected movie on the main page 4. pref_general.xml - added android id

--- app/src/androidTest/java/com/themartinsierra/popularmovieapp/ApplicationTest.java
+++ app/src/androidTest/java/com/themartinsierra/popularmovieapp/ApplicationTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import android.support.test.rule.ActivityTestRule;
@@ -16,6 +18,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -32,6 +35,18 @@ public class ApplicationTest  {
     //tests a click on fragment_movie.xml's gridview.
     @Test
     public void test () {
+        onView(ViewMatchers.withId(R.id.movies_grid));
         onView(withId(R.id.movies_grid)).perform(click());
+        try {
+            Thread.sleep(10000, 1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(ViewMatchers.withId(R.id.movieposter_image));
+        onView(withId(R.id.originaltitle_text)).check(matches(withText("Mechanic: Resurrection")));
+        onView(withId(R.id.releasedate_text)).check(matches(withText("2016-08-25")));
+        onView(withId(R.id.voteaverage_text)).check(matches(withText("4.28")));
+        onView(withId(R.id.overview_text)).check(matches(withText("Arthur Bishop thought he had put his murderous past behind him when his most formidable foe kidnaps the love of his life. Now he is forced to travel the globe to complete three impossible assassinations, and do what he does best, make them look like accidents.")));
+
     }
 }
